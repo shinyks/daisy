8597d301da0c976c5ac99c1b5e24016f22d9e02d
shinyks <syk2932@naver.com>
2023-12-27 16:21:34 +0900

    feat: Add POST method

--- src/request/index.ts
+++ src/request/index.ts
@@ -3,7 +3,7 @@ import xml2js from 'xml2js';
 /**
  * Fetch data
  * @param url URL to fetch
- * @param cache cache
+ * @param cache Cache
  * @returns Data
  */
 const get = async (url: string, cache: boolean = false): Promise<Response | null> => {
@@ -15,9 +15,33 @@ const get = async (url: string, cache: boolean = false): Promise<Response | null
 };
 
 /**
+ * POST data
+ * @param url URL to fetch
+ * @param data JSON data
+ * @param cache Cache
+ * @returns Data
+ */
+const post = async (url: string, data: any, cache: boolean = false): Promise<Response | null> => {
+  try {
+    const options: RequestInit = {
+      method: 'POST',
+      cache: cache ? undefined : 'no-cache',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    return await fetch(url, options);
+  } catch (error) {
+    return null;
+  }
+};
+
+/**
  * Fetch data as string
  * @param url URL to fetch
- * @param cache cache
+ * @param cache Cache
  * @returns Text data
  */
 export const getText = async (url: string, cache: boolean = false): Promise<any> => {
@@ -30,7 +54,7 @@ export const getText = async (url: string, cache: boolean = false): Promise<any>
 /**
  * Fetch data as JSON object
  * @param url URL to fetch
- * @param cache cache
+ * @param cache Cache
  * @returns JSON data
  */
 export const getJson = async (url: string, cache: boolean = false): Promise<any> => {
@@ -43,7 +67,7 @@ export const getJson = async (url: string, cache: boolean = false): Promise<any>
 /**
  * Fetch data as XML object
  * @param url URL to fetch
- * @param cache cache
+ * @param cache Cache
  * @returns XML data
  */
 export const getXml = async (url: string, cache: boolean = false): Promise<any> => {
@@ -51,4 +75,18 @@ export const getXml = async (url: string, cache: boolean = false): Promise<any> 
   const xmlObject = responseText ? await xml2js.parseStringPromise(responseText, { strict: false, trim: true }) : null;
 
   return xmlObject;
+};
+
+/**
+ * Fetch data as JSON object
+ * @param url URL to fetch
+ * @param data JSON data
+ * @param cache Cache
+ * @returns Data
+ */
+export const postJson = async (url: string, data: any, cache: boolean = false): Promise<any> => {
+  const response = await post(url, data, cache);
+  const responseJson = response && response.ok ? await response.json() : null;
+
+  return responseJson;
 };
